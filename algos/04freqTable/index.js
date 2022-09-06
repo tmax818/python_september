77a9d23d19ed16bc7c601de68428e1b0eea1452e
tmax818 <tylermaxwell661@gmail.com>
2022-09-06 14:59:16 -0700

    update algo

--- algos/04freqTable/index.js
+++ algos/04freqTable/index.js
@@ -2,12 +2,16 @@
   Given an array of strings
   return the number of times each string occurs (a frequency / hash table)
 */
-// {a: 3}
+// a : 3
 const arr1 = ["a", "a", "a"];
 const expected1 = {
   a: 3,
 };
-
+// a : 2
+// b : 1
+// c : 3
+// B : 1
+// d : 1
 const arr2 = ["a", "b", "a", "c", "B", "c", "c", "d"];
 const expected2 = {
   a: 2,
@@ -28,4 +32,30 @@ const expected3 = {};
  * @returns {Object<string, number>} A frequency table where the keys are items
  *    from the given arr and the values are the amnt of times that item occurs.
  */
-function makeFrequencyTable(arr) {}
+function makeFrequencyTable(arr) {
+  // create the object to hold the records
+  const obj = {}
+  // loop over the array
+  for(let i = 0; i < arr.length; i++){
+    // have I seen this character: 
+    // if yes, I have seen it: increment my count for that character
+    if(obj[arr[i]]){
+      obj[arr[i]] += 1
+      // if no, create a new record for that character and set the number to 1
+    } else {
+      obj[arr[i]] = 1
+    }
+  }
+  console.log(obj)
+  return obj
+// how do you get to the 'a' inside a loop?????
+
+}
+
+makeFrequencyTable(arr1)
+makeFrequencyTable(arr2)
+makeFrequencyTable(arr3)
+
+
+
+
